a0aefe349f2d5fc3ef2e03191b16bad51478097f
Marcelo <Marcelo14axl@gmail.com>
2022-08-29 22:50:20 -0300

    Brincando com commit do git e JavaScript

--- aula03/script3_02.js
+++ aula03/script3_02.js
@@ -1,6 +1,11 @@
  let aluno, nota;
  aluno = "Astrogildo";
  nota = "9.5";
- document.write ("aluno: ",aluno);
- document.write ("<br>");
+ document.write (
+    `
+    Aluno: ${aluno} <hr>
+    Nota: ${nota}
+    `
+ );
+ document.write ("<br><hr>");
  document.write ("nota: ", nota);
--- aula03/script3_04.js
+++ aula03/script3_04.js
@@ -1,5 +1,11 @@
 let dia = 30;
 let mes = "Agosto";
 let ano = 2022;
-let datacompleta = dia + " de " + mes + " de " + ano;
-document.write ("Data:" + datacompleta);
+let dataCompleta = `${dia} de ${mes} de ${ano}`;
+let botaoResultado = document.querySelector('.button');
+let dataDoc = document.querySelector('.data');
+console.log(botaoResultado);
+
+botaoResultado.addEventListener('click', () => {
+    dataDoc.innerHTML = `Data: ${dataCompleta}`;
+})
